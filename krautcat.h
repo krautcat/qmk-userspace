41225393903ba09139716a1fcfa90769ad0da954
Georgiy Odisharia <georgiy.odisharia@gmail.com>
2023-06-25 22:28:54 +0300

    [keycodes] Added different hold-tap keycodes
    Signed-off-by: Georgiy Odisharia <georgiy.odisharia@gmail.com>

--- krautcat.h
+++ krautcat.h
@@ -34,7 +34,7 @@ enum krautcat_common_keycodes {
     RGUI_F12 = RGUI_T(KC_F12),      // Tap is F12, hold is Right GUI. 
 
     
-    LCTL_TAB = LCTL_T(KC_TAB),	    // Tab is tap, hold is Left Control.
+    LCTL_TAB = LCTL_T(KC_TAB),      // Tab is tap, hold is Left Control.
     LCTL_ENT = LCTL_T(KC_ENT),      // Tap is Enter, hoid is Left Control.
     LCTL_GRV = LCTL_T(KC_GRV),      // Tapi is Grave, hold is Left Control.
     LCTL_PLUS = RCTL_T(KC_PLUS),    // Tap is Plus sign, hold is Left Control.
@@ -42,20 +42,25 @@ enum krautcat_common_keycodes {
     RCTL_ENT = RCTL_T(KC_ENT),      // Enter is tap, hold is Right Control.
     RCTL_QTE = RCTL_T(KC_QUOTE),    // Tap is Quotation Signs, hold is Right Control.
     RCTL_UNDS = RCTL_T(KC_UNDS),    // Tap is Underscore, hold is Right Control.
-    
+   
+
+    LALT_GRV = LALT_T(KC_GRV),      // Tap is Grave, hold is Left Alt.
     LALT_ESC = LALT_T(KC_ESC),      // Esc is tap, hold is Left Alt.
     LALT_TAB = LALT_T(KC_TAB),      // Tap is Tab, hold is Left Alt.
     LALT_BSLS = LALT_T(KC_BSLS),    // Tap is Backslash, hold is Left Alt.
-    LALT_EQL = LALT_T(KC_EQL),		// Gap is Equal, hold is Left Alt.
-  
+    LALT_EQL = LALT_T(KC_EQL),      // Gap is Equal, hold is Left Alt.
+
     RALT_GRV = RALT_T(KC_GRV),      // Grave is tap, hold is Right Alt.
     RALT_MNS = RALT_T(KC_MINUS),    // Tap is Minus, hold is Right Alt.
     RALT_RBRC = RALT_T(KC_RBRC),    // Tap is Right Bracket, hold is Left Alt.
+    RALT_F12 = RALT_T(KC_F12),      // Tap is F12, hold is Right Alt.
 
 
     LSFT_SPC = LSFT_T(KC_SPC),      // Space is tap, hold is Left Shift.
- 
+    LSFT_BSLS = LSFT_T(KC_BSLS),
+    
     RSFT_BSPC = RSFT_T(KC_BSPC),    // Backspace is tap, hold is Right Shift.
+    RSFT_MNS = RSFT_T(KC_MINUS),    // Tap is Minus, hold is Right Shift.
 };
 
 enum krautcat_layer_keycodes {
